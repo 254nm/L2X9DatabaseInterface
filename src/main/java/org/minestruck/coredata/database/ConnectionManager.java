408a85faedf2392f74188c34ff6bb758fdf6b6c0
Alex Sobiek <alex@alexsobiek.com>
2023-06-27 23:57:03 -0500

    Added Redis

--- src/main/java/org/minestruck/coredata/database/ConnectionManager.java
+++ src/main/java/org/minestruck/coredata/database/ConnectionManager.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import org.minestruck.coredata.MCServer;
 import org.minestruck.coredata.database.connections.MySQL;
+import org.minestruck.coredata.database.connections.Redis;
 
 /**
  * @author 254n_m
@@ -14,8 +15,17 @@ import org.minestruck.coredata.database.connections.MySQL;
 @Accessors(fluent = true)
 public class ConnectionManager implements AutoCloseable {
     private final MySQL mysql;
+    private final Redis redis;
+
     public ConnectionManager(MCServer server) {
-        mysql = new MySQL(System.getenv("MYSQL_CONNECTION_STRING"), System.getenv("MYSQL_USERNAME"), System.getenv("MYSQL_PASSWORD"), server);
+        mysql = new MySQL(getEnv("MYSQL_CONNECTION_STRING"), getEnv("MYSQL_USERNAME"), getEnv("MYSQL_PASSWORD"), server);
+        redis = new Redis(getEnv("REDIS_HOST"), Integer.parseInt(getEnv("REDIS_PORT")), getEnv("REDIS_USER"), getEnv("REDIS_PASS"), server);
+    }
+
+    private static String getEnv(String name) {
+        String value = System.getenv(name);
+        if (value == null) throw new RuntimeException("Environment variable " + name + " is not set!");
+        return value;
     }
 
     @Override
---
+++ src/main/java/org/minestruck/coredata/database/connections/Redis.java
@@ -0,0 +1,87 @@
+package org.minestruck.coredata.database.connections;
+
+import org.minestruck.coredata.MCServer;
+import org.minestruck.coredata.database.Database;
+import redis.clients.jedis.BinaryJedisPubSub;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+public class Redis implements Database {
+    private MCServer server;
+    private JedisPool pool;
+
+    public Redis(String hostname, int port, String user, String password, MCServer server) {
+        try {
+            this.pool = new JedisPool(hostname, port, user, password);
+            this.server = server;
+        } catch (Throwable t) {
+            server.logger().error("Failed to setup Redis", t);
+            server.shutdownServer();
+        }
+    }
+
+    private Jedis getResource() {
+        try (Jedis jedis = this.pool.getResource()) {
+            return jedis;
+        } catch (Throwable t) {
+            server.logger().error("Failed to get resource from pool", t);
+            server.shutdownServer();
+            return null;
+        }
+    }
+
+    public void set(String key, String value) {
+        this.getResource().set(key, value);
+    }
+
+    public void unset(String key, String value) {
+        this.getResource().del(key);
+    }
+
+    public String get(String key) {
+        return this.getResource().get(key);
+    }
+
+    public void publish(String channel, byte[] data) {
+        this.getResource().publish(channel.getBytes(StandardCharsets.UTF_8), data);
+    }
+
+    public void publish(String channel, ByteBuffer data) {
+        this.publish(channel, data.array());
+    }
+
+    public AutoCloseable subscribe(List<String> channels, BiConsumer<String, ByteBuffer> callback) {
+        byte[][] channelsBytes = channels.stream().map(String::getBytes).toList().toArray(new byte[0][0]);
+
+        BinaryJedisPubSub sub = new BinaryJedisPubSub() {
+            @Override
+            public void onMessage(byte[] channel, byte[] message) {
+                callback.accept(new String(channel, StandardCharsets.UTF_8), ByteBuffer.wrap(message));
+            }
+        };
+
+        CompletableFuture.runAsync(() -> {
+            try {
+                this.getResource().subscribe(sub, channelsBytes);
+            } catch (Throwable t) {
+                server.logger().error("Failed to subscribe to redis channels", t);
+                server.shutdownServer();
+            }
+        });
+
+        return () -> sub.unsubscribe(channelsBytes);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (this.pool != null && !this.pool.isClosed()) {
+            this.pool.close();
+        }
+    }
+}

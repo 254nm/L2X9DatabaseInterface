f4611a9df688368f0d45addd16402524325a27ba
Mason Redacted <67813185+254nm@users.noreply.github.com>
2023-06-28 01:22:54 -0400

    Remove duplicated message

--- src/main/java/org/minestruck/coredata/database/connections/MySQL.java
+++ src/main/java/org/minestruck/coredata/database/connections/MySQL.java
@@ -30,7 +30,6 @@ public class MySQL implements Database {
         } catch (Throwable t) {
             server.logger().error("Failed to connect to mysql database due to {} please see stacktrace below for more info!", t.getClass().getSimpleName());
             t.printStackTrace();
-            server.logger().error("Failed to connect to mysql database due to %s please see stacktrace below for more info!", t);
             server.shutdownServer();
         }
     }
